eba5a163eb10c983f5dffcd71019f952f93be14f
veapach <egor_chapaev@bk.ru>
2024-06-29 23:02:44 +0300

    добавил pip requirements в файл txt

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 from app.handlers import router
 from app.database.models import async_main
 
+#Перед запуском вписать в консоль - pip install -r requirements.txt
+
 async def main():
     await async_main()
     load_dotenv()
